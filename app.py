50bc710b49444b557aba95fc19f619435fbdc72a
Taylor Putman <fishputman@gmail.com>
2025-09-23 18:28:24 -0400

    added comments to start flask app

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from app import create_app
 from app.models import db
 
 app = create_app("DevelopmentConfig")
+# Start virtual environment for Mac
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy alembic
+# pip freeze > requirements.txt
 
 
 with app.app_context():
