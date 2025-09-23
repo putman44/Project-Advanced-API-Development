546af1059cfce9d5e8605bf238d65678ff059dd4
Taylor Putman <fishputman@gmail.com>
2025-09-23 18:29:20 -0400

    added blueprints for  mechanics and service_tickets

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from .extensions import ma
 from .models import db
 from .blueprints.customers import customers_bp
+from .blueprints.mechanics import mechanics_bp
+from .blueprints.service_tickets import service_tickets_bp
 
 
 def create_app(config_name):
@@ -16,5 +18,7 @@ def create_app(config_name):
 
     # Register Blueprints
     app.register_blueprint(customers_bp, url_prefix="/customers")
+    app.register_blueprint(mechanics_bp, url_prefix="/mechanics")
+    app.register_blueprint(service_tickets_bp, url_prefix="/service_tickets")
 
     return app
