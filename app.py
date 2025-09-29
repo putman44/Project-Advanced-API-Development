94c431fe99c46de9f9e6f5d5f754ae47df951901
Taylor Putman <fishputman@gmail.com>
2025-09-29 09:45:03 -0400

    added flask-caching and flask-limiter

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from app.models import db
 app = create_app("DevelopmentConfig")
 # Start virtual environment for Mac
 # python3 -m venv venv
-# source venv/bin/activate
-# pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy alembic
+# source .venv/bin/activate
+# pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy Flask-Limiter Flask-Caching alembic
 # pip freeze > requirements.txt
 
 
