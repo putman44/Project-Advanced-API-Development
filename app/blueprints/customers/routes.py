2faa0ea958d3178ca36962645fbf531140570e53
Taylor Putman <fishputman@gmail.com>
2025-09-30 12:06:51 -0400

    added login route /customer/my-tickets route added token_required to customer routes

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -1,9 +1,40 @@
-from .schemas import customer_schema, customers_schema
+from .schemas import customer_schema, customers_schema, login_schema
+from app.blueprints.service_tickets.schemas import service_tickets_schema
 from flask import request, jsonify
 from marshmallow import ValidationError
 from sqlalchemy import select
-from app.models import Customer, db
+from app.models import Customer, ServiceTicket, db
 from . import customers_bp
+from app.utils.utils import encode_token, token_required
+
+
+@customers_bp.route("/login", methods=["POST"])
+def login():
+    try:
+        customer_credentials = login_schema.load(request.json)
+        email = customer_credentials["email"]
+        password = customer_credentials["password"]
+    except ValidationError as e:
+        return jsonify(e.messages)
+
+    query = select(Customer).where(Customer.email == email)
+    customer = db.session.execute(query).scalars().first()
+
+    if customer and customer.password == password:
+        auth_token = encode_token(
+            customer.user_uuid,
+            customer.role,
+            customer.token_version,
+        )
+
+        response = {
+            "status": "success",
+            "message": "Successfully Logged In",
+            "auth_token": auth_token,
+        }
+        return jsonify(response), 200
+    else:
+        return jsonify({"messages": "Invalid email or password"}), 401
 
 
 @customers_bp.route("/", methods=["POST"])
@@ -38,35 +69,31 @@ def get_customer(customer_id):
     return jsonify({"error": "Customer not found."}), 404
 
 
-@customers_bp.route("/<int:customer_id>", methods=["PUT"])
-def update_customer(customer_id):
-    customer = db.session.get(Customer, customer_id)
-    if not customer:
-        return jsonify({"error": "Customer not found"}), 404
+@customers_bp.route("/my-tickets", methods=["GET"])
+@token_required
+def get_my_tickets(user, user_role):
+    service_tickets = user.service_tickets
 
-    try:
-        data = customer_schema.load(request.json, partial=True)
-        for key, value in data.items():
-            setattr(customer, key, value)
-        db.session.commit()
-        return customer_schema.jsonify(customer), 200
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({"error": str(e)}), 400
+    return service_tickets_schema.jsonify(service_tickets), 200
 
 
-@customers_bp.route("/<int:customer_id>", methods=["DELETE"])
-def delete_customer(customer_id):
-    customer = db.session.get(Customer, customer_id)
+@customers_bp.route("/", methods=["PUT"])
+@token_required
+def update_customer(user, user_role):
 
-    if not customer:
-        return jsonify({"error": "Customer not found."}), 404
+    data = customer_schema.load(request.json, partial=True)
+    for key, value in data.items():
+        setattr(user, key, value)
+    db.session.commit()
+    return customer_schema.jsonify(user), 200
 
-    db.session.delete(customer)
+
+@customers_bp.route("/", methods=["DELETE"])
+@token_required
+def delete_customer(user, user_role):
+    db.session.delete(user)
     db.session.commit()
     return (
-        jsonify({"message": f"Customer id: {customer_id}, successfully deleted."}),
+        jsonify({"message": f"Customer id: {user.id}, successfully deleted."}),
         200,
     )
