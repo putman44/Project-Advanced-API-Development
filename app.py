94c86e10cd84ab6fe7a915c097b2746e2bea7abb
Taylor Putman <fishputman@gmail.com>
2025-10-01 19:25:54 -0400

    added python3 -m venv .venv --copies

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app.models import db
 
 app = create_app("DevelopmentConfig")
 # Start virtual environment for Mac
-# python3 -m venv .venv
+# python3 -m venv .venv --copies
 # source .venv/bin/activate
 # pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy Flask-Limiter Flask-Caching python-jose alembic
 # pip freeze > requirements.txt
