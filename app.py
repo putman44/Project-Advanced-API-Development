3f169f53f445c01326b3c88d3307a591c7fb8077
Taylor Putman <fishputman@gmail.com>
2025-10-07 09:27:31 -0400

    updated imports and added instructions for watch-swagger nodemon

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ app = create_app("DevelopmentConfig")
 # Start virtual environment for Mac
 # python3 -m venv .venv --copies
 # source .venv/bin/activate
-# pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy Flask-Limiter Flask-Caching python-jose alembic
+# pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy Flask-Limiter Flask-Caching python-jose alembic flask-swagger flask_swagger_ui @apidevtools/swagger-cli nodemon
 # pip freeze > requirements.txt
+
+# for the merged yaml files run npm run watch-swagger in virtual environment,
+# this will save the changes to other .yaml files and then add them to the merged.yaml file
+
+# run app.py in seperate terminal
 
 
 with app.app_context():
